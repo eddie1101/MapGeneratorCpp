c2767ebd0480b28f7eda01614e040622efb2bcb4
Eddie <erg1101@outlook.com>
2024-09-09 20:17:59 -0400

    VSCode doesn't like the operator overloads but this is a working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200,200), "SFML Works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	const int WIDTH = 1920;
+	const int HEIGHT = 1080; 
+	RenderWindow window(VideoMode(1920,1080), "SFML Works!");
+	RectangleShape rect(Vector2f(200, 100));
+	rect.setPosition(WIDTH / 2, HEIGHT / 2);
+	rect.setFillColor(Color::Green);
 
+	window.setFramerateLimit(120);
+
+	float moveAmt = 2.4;
 	while(window.isOpen()){
-		sf::Event event;
+		Event event;
 		while(window.pollEvent(event)) {
-			if(event.type == sf::Event::Closed) {
+			if(event.type == Event::Closed) {
 				window.close();
 			}
 		}
 
+		Vector2f dV;
+		if(Keyboard::isKeyPressed(Keyboard::A)) {
+			dV += Vector2f(-moveAmt, 0);
+		}
+		if(Keyboard::isKeyPressed(Keyboard::D)) {
+			dV += Vector2f(moveAmt, 0);
+		}
+		if(Keyboard::isKeyPressed(Keyboard::S)) {
+			dV += Vector2f(0, moveAmt);
+		}
+		if(Keyboard::isKeyPressed(Keyboard::W)) {
+			dV += Vector2f(0, -moveAmt);
+		}
+
+		rect.setPosition(dV + rect.getPosition());
+
 		window.clear();
-		window.draw(shape);
+		window.draw(rect);
 		window.display();
 	}
 	
